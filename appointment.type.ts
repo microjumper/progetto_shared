08084901921526be7e1aab7c8a6565ec20a80b02
Salvatore Sammarco <microjmpr@gmail.com>
2024-06-08 19:43:57 +0200

    update shared typing

--- appointment.type.ts
+++ appointment.type.ts
@@ -1,5 +1,11 @@
+import { FileMetadata } from "./fileMetadata.type";
+
 export interface Appointment {
+  id?: string
   legalServiceId?: string;
   legalServiceTitle?: string;
+  eventId?: string;
   eventDate?: string;
+  accountId?: string;
+  fileMetadata?: FileMetadata[];
 }
---
+++ fileMetadata.type.ts
@@ -0,0 +1,6 @@
+export interface FileMetadata {
+  originalFileName: string;
+  fileUrl: string;
+  accountId: string;
+  accountEmail: string;
+}
